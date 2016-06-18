9af27f53de3285565eb6c73f4e500a5aece3713c
Adam Rogers <adam@arcticanaudio.com>
2016-06-18 19:45:50 +0200

    Updated version to 0.0.3

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,8 +40,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Waveformly";
-    const char* const  versionString  = "0.0.1";
-    const int          versionNumber  = 0x1;
+    const char* const  versionString  = "0.0.3";
+    const int          versionNumber  = 0x3;
 }
 #endif
 
